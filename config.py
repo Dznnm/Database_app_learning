4507fce1eda35428535d0f2db1732a8df0a6e0d3
Dznnm <danun.speed@gmail.com>
2026-06-05 05:36:55 +0700

    PostGreSQL connected to learning app

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'superkey123'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///database.db'
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg://postgres:postgresql17@localhost:5432/inventory_app"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REGISTER_TOKEN = os.environ.get('REGISTER_TOKEN') or 'supertoken123'
