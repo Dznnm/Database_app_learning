ece083ae138183b06955a1ca0ff848475fa275b7
Dznnm <danun.speed@gmail.com>
2026-06-08 19:54:51 +0700

    added print if not SQLalchemy database temporary

--- config.py
+++ config.py
@@ -6,5 +6,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if not SQLALCHEMY_DATABASE_URI:
+        raise RuntimeError("DATABASE_URL is missing")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REGISTER_TOKEN = os.environ.get('REGISTER_TOKEN')
