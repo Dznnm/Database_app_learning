1fd5a9e70938e8ab994d250fa5aee5458b93845e
Dznnm <danun.speed@gmail.com>
2026-06-08 20:07:16 +0700

    fix postgresql driver paksa psycopg

--- config.py
+++ config.py
@@ -3,10 +3,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+database_url = os.environ.get("DATABASE_URL")
+
+if not database_url:
+    raise RuntimeError("DATABASE_URL is missing")
+
+if database_url.startswith("postgresql://"):
+    database_url = database_url.replace(
+        "postgresql://",
+        "postgresql+psycopg://",
+        1
+    )
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    if not SQLALCHEMY_DATABASE_URI:
-        raise RuntimeError("DATABASE_URL is missing")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
+    SQLALCHEMY_DATABASE_URI = database_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    REGISTER_TOKEN = os.environ.get('REGISTER_TOKEN')
+    REGISTER_TOKEN = os.environ.get("REGISTER_TOKEN")
